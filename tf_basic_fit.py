57b233c4fbecdf61210aeaaa794ee637a443ce6e
pyaster <pyaster4@gmail.com>
2017-04-28 18:11:22 +0200

    Making vectorial approact for Linear Regression

--- tf_basic_fit.py
+++ tf_basic_fit.py
@@ -50,10 +50,82 @@ def test_fit(FLAGS):
 
     foo = 1
 
+
+def test_fit_theta(FLAGS):
+    "Basic Linear Regressions with implicit named variables (W, b)"
+    tf.reset_default_graph()
+
+    # Create the input data
+    Theta_r = tf.constant([1.2, 0.25],  name="Theta_r", shape=(1, 2))
+    X = tf.random_uniform([5], 0.0, 10)
+    X = tf.reshape(X, [1, -1])
+    bias = tf.ones_like(X)
+
+    X = tf.concat([bias, X], 0)
+    X = tf.Variable(X, trainable=False, name='X')
+
+    Y_r = tf.matmul(Theta_r, X)
+
+    # Inference Graph
+    with tf.name_scope('inference'):
+        # Create the model
+        Theta = tf.Variable(tf.random_uniform([1, 2], 0.0, 1.0), name='Theta')
+        Y = tf.matmul(Theta, X)
+        variable_summaries(Theta, 'Theta')
+
+    # Training Graph
+    with tf.name_scope('train'):
+        # training
+        v_loss = tf.square(Y - Y_r)
+        loss = tf.reduce_mean(v_loss)
+
+        # optimizer = tf.train.GradientDescentOptimizer(0.005)
+        optimizer = tf.train.AdamOptimizer()
+
+        step = optimizer.minimize(loss)
+
+        tf.summary.scalar('loss', loss)
+
+    # Create a session
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+
+    train_writer = tf.summary.FileWriter(FLAGS.log_dir + '/vectorialFit',
+                                         sess.graph)
+
+    merged = tf.summary.merge_all()
+    err0 = 10**6
+    for i in xrange(10000):
+        sess.run(step)
+
+        if not i % 50:
+            summary = sess.run(merged)
+            train_writer.add_summary(summary, i)
+
+            print('-'*40)
+            print("Iteration: %d" % i)
+            print("Theta : \t%s" % sess.run(Theta))
+            print("Theta*: \t%s" % sess.run(Theta_r))
+
+            err = sess.run(loss)
+            print("Error:\t\t%s" % err)
+
+            r = (err0 - err) / err0
+            print("Rel.Error:\t%s" % r)
+            if err < 1e-06 or r < 0.001:
+                break
+            err0 = err
+
+    train_writer.close()
+
+    foo = 1
+
 if __name__ == '__main__':
     FLAGS, unparsed = cmd_parser()
     log_dir(FLAGS)
 
+    test_fit_theta(FLAGS)
     test_fit(FLAGS)
+
 
 
